1d676a16d6961600354265156d90abcdf3247a14
Harsh Agarwal <agrawal.harsh14@gmail.com>
2019-10-23 23:49:47 +0530

    Modified DSA lab BST program

--- DSA_Lab/Lab7/bst.cpp
+++ DSA_Lab/Lab7/bst.cpp
@@ -139,7 +139,7 @@ TreeNode* predecessor(TreeNode* root, int data) {
 void preorder(TreeNode* root){
 	if(root == NULL)
 		return;
-	cout<<root->val<<endl;
+	cout<<root->val<<" ";
 	preorder(root->left);
 	preorder(root->right);
 }
@@ -162,60 +162,134 @@ void leveltraversal(TreeNode* root){
 
 int main(){
 	TreeNode* root = NULL;
-	cout<<"Enter the number of elements to be inserted in the tree: ";
-	int n;
-	cin>>n;
-	cout<<"Enter the elements: ";
-	while(n--){
-		int k;
-		cin>>k;
-		root = insert(root,k);
-	}
+	int choice;
 
-	cout<<"The max. element is "<<findmax(root)->val<<endl;
+	// cout<<"Enter the number of elements to be inserted in the tree: ";
+	// int n;
+	// cin>>n;
+	// cout<<"Enter the elements: ";
+	// while(n--){
+	// 	int k;
+	// 	cin>>k;
+	// 	root = insert(root,k);
+	// }
 
-	cout<<"The min. element is "<<findmin(root)->val<<endl;
-	cout<<"-----------------------------------"<<endl;
+	// cout<<"The max. element is "<<findmax(root)->val<<endl;
+
+	// cout<<"The min. element is "<<findmin(root)->val<<endl;
+	// cout<<"-----------------------------------"<<endl;
 	 
-	 cout<<"Enter the number to be searched\n";
-	 int num;
-	 cin>>num;
-	 if(search(root,num)== true)
-	 	cout<<"Found\n";
-	 else
-	 	cout<<"Not Found\n";
+	//  cout<<"Enter the number to be searched\n";
+	//  int num;
+	//  cin>>num;
+	//  if(search(root,num)== true)
+	//  	cout<<"Found\n";
+	//  else
+	//  	cout<<"Not Found\n";
 
-	 cout<<"Enter the number to be Deleted\n";
-	 int num1;
-	 cin>>num1;
-	 if(search(root,num1)== true)
-	 	Delete(root,num1);
-	 else
-	 	cout<<"This number is not found on the tree\n";
+	//  cout<<"Enter the number to be Deleted\n";
+	//  int num1;
+	//  cin>>num1;
+	//  if(search(root,num1)== true)
+	//  	Delete(root,num1);
+	//  else
+	//  	cout<<"This number is not found on the tree\n";
 	 
 
-	 cout<<"The max. element is "<<findmax(root)->val<<endl;
+	//  cout<<"The max. element is "<<findmax(root)->val<<endl;
 
-	 cout<<"The min. element is "<<findmin(root)->val<<endl;
+	//  cout<<"The min. element is "<<findmin(root)->val<<endl;
 	
-	preorder(root);
-	cout<<"---------------"<<endl;
-	leveltraversal(root);
-	cout<<endl;
+	// preorder(root);
+	// cout<<"---------------"<<endl;
+	// leveltraversal(root);
+	// cout<<endl;
 	
-	cout<<"Enter the number to find predecessor\n";
-	 int num2;
-	 cin>>num2;
-	 if(search(root,num2)== true)
-	 	cout<<"Predecessor of "<<num2<<" is: "<<predecessor(root,num2)->val<<endl;
-	 else
-	 	cout<<"This number is not found on the tree\n";
+	// cout<<"Enter the number to find predecessor\n";
+	//  int num2;
+	//  cin>>num2;
+	//  if(search(root,num2)== true)
+	//  	cout<<"Predecessor of "<<num2<<" is: "<<predecessor(root,num2)->val<<endl;
+	//  else
+	//  	cout<<"This number is not found on the tree\n";
 
-	 cout<<"Enter the number to find successor\n";
-	 int num3;
-	 cin>>num3;
-	 if(search(root,num3)== true)
-	 	cout<<"Successor of "<<num3<<" is: "<<successor(root,num3)->val<<endl;
-	 else
-	 	cout<<"This number is not found on the tree\n";
+	//  cout<<"Enter the number to find successor\n";
+	//  int num3;
+	//  cin>>num3;
+	//  if(search(root,num3)== true)
+	//  	cout<<"Successor of "<<num3<<" is: "<<successor(root,num3)->val<<endl;
+	//  else
+	//  	cout<<"This number is not found on the tree\n";
+
+	 do
+    {
+        cout<<"Select Yout Choice\n";
+		cout<<" 1. Insert\n 2. Search\n 3. Max element\n 4. Min element\n 5. successor\n 6. predecessor\n 7. Delete\n 8. Preorder Traversal Print\n 9.Level Traversal Print\n";
+        cin>>choice;
+        switch (choice)
+        {
+            case 1:
+                cout<<"Enter the number of elements to be inserted in the tree: ";
+				int n;
+				cin>>n;
+				cout<<"Enter the elements: ";
+				while(n--){
+					int k;
+					cin>>k;
+					root = insert(root,k);
+				}
+                break;
+            case 2:
+            	int x;
+                cout << "Enter the element to be searched \n";
+    			cin>>x;
+    			if(search(root,x)){
+    				cout<<"The element is present\n";
+
+   				}
+   				else
+    				cout<<"The element is not present\n";
+
+                break;
+            case 3:
+               	cout<<"The max. element is: "<<findmax(root)->val<<endl;
+                break;
+            case 4:
+               	cout<<"The min. element is: "<<findmin(root)->val<<endl;
+                break;
+            case 5:
+            	int k;
+            	cout<<"Enter the key to find successor: \n";
+            	cin>>k;
+
+               	cout<<"The successor element is: "<<successor(root,k)->val<<endl;
+                break;
+            case 6:
+            	int k1;
+            	cout<<"Enter the key to find predecessor: \n";
+            	cin>>k1;
+
+               	cout<<"The predecessor element is: "<<predecessor(root,k)->val<<endl;
+                break;
+            case 7:
+            	int d;
+				cout<<"Enter the key to be deleted\n";
+				cin>>d;
+				root = Delete(root, d);
+                break;
+            case 8:
+            	cout << "Preorder traversal of the BST is \n";  
+    			preorder(root);
+    			cout<<"\n";
+    			break;
+    		case 9:
+            	cout << "Level order traversal of the BST is \n";  
+    			leveltraversal(root);
+    			cout<<"\n";
+    			break;
+            default:
+               cout<<"WRONG CHOICE";
+        }
+    } while (choice != 0);
+
 }
